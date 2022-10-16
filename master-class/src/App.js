c0b91f4bf4ffb26b85d5f4b749f0c7ddb660b93e
weeeeey <qser155@naver.com>
2022-10-16 13:57:29 +0900

    #2 styled-components className

--- master-class/src/App.js
+++ master-class/src/App.js
@@ -7,10 +7,14 @@ const Box = styled.div`
     background-color: teal;
     width: 100px;
     height: 100px;
+    &.Son {
+        background-color: red;
+    }
 `;
 const App = () => {
     return (
         <Father>
+            <Box className="Son"></Box>
             <Box></Box>
             <Box style={{ background: "blue" }}></Box>
         </Father>
