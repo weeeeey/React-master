1eaaacfd7e68c450857feea7a5458eeda97a3149
weeeeey <qser155@naver.com>
2022-10-16 16:40:37 +0900

    #2 style-components ThemeProvider를 통한 darkMode,lightMode

--- master-class/src/App.js
+++ master-class/src/App.js
@@ -1,67 +1,50 @@
-import styled, { keyframes } from "styled-components";
+// 다크 모드의 50%는 themes 에 관련되어 있음
+// themes 기본적으로 color를 가지고있음
+// 그래서 나중에 색을 바꾸고 싶을떄
+// 컴포넌트의 색을 일일이 바꾸는게 아니라
+// object의 값만 바꿔주면 됨
 
+import { useState } from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+    textColor: "whitesmoke",
+    backgroundColor: "#111",
+};
+// 다크,라이틍 속성은 같아야함
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "#whitesmoke",
+};
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
     display: flex;
-`;
-const rotationAnimation = keyframes`
-    /* from {
-        transform:rotate(0deg);
-        border-radius:0px;
-    }
-    to {
-        transform:rotate(360deg);
-        border-radius:100px;
-    } */
-    0%{
-        transform:rotate(0deg);
-        border-radius:0px;
-    }
-    50%{
-        transform:rotate(360deg);
-        border-radius:100px;
-    }
-    100%{
-        transform:rotate(0deg);
-        border-radius:0px;
-    }
-`;
-const Emoji = styled.span`
-    font-size: 36px;
-    &:active {
-        opacity: 0;
-    }
-`;
-
-const Box = styled.div`
-    display: flex;
-    height: 200px;
-    width: 200px;
-    background-color: tomato;
+    height: 100px;
+    width: 100px;
     justify-content: center;
     align-items: center;
-    animation: ${rotationAnimation} 5s linear infinite;
-    /* 특정 selector 사용하기 */
-    /* targeting */
-    ${Emoji} {
-        &:hover {
-            font-size: 72px;
-        }
-    }
+    background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const App = () => {
+    const [mode, setMode] = useState("dark");
+    const decideMode = () => {
+        if (mode === "dark") {
+            setMode("light");
+        } else {
+            setMode("dark");
+        }
+    };
     return (
-        <div>
+        <ThemeProvider theme={mode === "dark" ? darkTheme : lightTheme}>
             <Wrapper>
-                <Box>
-                    {/* Box안을 단순하게 Emoji로 했다면 as를 사용해쓸시 적용 안됨
-                    하지만 ${tagName} 를 사용한다면 적용 가능 */}
-                    <Emoji as="div">ggg</Emoji>
-                </Box>
+                <Title onClick={decideMode}>Hello</Title>
             </Wrapper>
-            {/* Box 밖이므로 hover 적용 안됨 */}
-            <Emoji>sdsd</Emoji>
-        </div>
+        </ThemeProvider>
     );
 };
 
