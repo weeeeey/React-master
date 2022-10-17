a74e252bf162a8dc06e2fa5f0d4fcea603a5957d
weeeeey <qser155@naver.com>
2022-10-17 14:01:32 +0900

    #3 TypeScript- Theme: Default Theme

--- typescript-tutorial/src/index.tsx
+++ typescript-tutorial/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { lightTheme, darkTheme } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={darkTheme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
 
