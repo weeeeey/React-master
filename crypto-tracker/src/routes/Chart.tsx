9e33156d66489d8b83615b9160f497e4edf3efe4
weeeeey <qser155@naver.com>
2022-10-20 16:49:26 +0900

    #4Crypto: Chart API - Apexchart
    import Apexchart from "react-apexcharts";
    
    yarn add react-apexcharts

--- crypto-tracker/src/routes/Chart.tsx
+++ crypto-tracker/src/routes/Chart.tsx
@@ -1,15 +1,69 @@
 import { useQuery } from "react-query";
 import { fetchChart } from "../api";
+import Apexchart from "react-apexcharts";
 
 interface IcoinId {
     coinId: string;
 }
+interface IchartData {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
 const Chart = ({ coinId }: IcoinId) => {
-    const { isLoading, data } = useQuery(["chart", coinId], () =>
+    const { isLoading, data } = useQuery<IchartData[]>(["chart", coinId], () =>
         fetchChart(coinId)
     );
-    console.log(data);
-    return <h1>Chart</h1>;
+    return (
+        <>
+            {isLoading ? (
+                "Loading..."
+            ) : (
+                <Apexchart
+                    type="line"
+                    series={[
+                        {
+                            name: "Price",
+                            data: data?.map((price) =>
+                                parseFloat(price.close)
+                            ) as number[],
+                        },
+                    ]}
+                    options={{
+                        theme: {
+                            mode: "dark",
+                        },
+                        chart: {
+                            height: 300,
+                            width: 500,
+                            toolbar: {
+                                show: false,
+                            },
+                            background: "transparent",
+                        },
+                        grid: { show: false },
+                        stroke: {
+                            curve: "smooth",
+                            width: 4,
+                        },
+                        yaxis: {
+                            show: false,
+                        },
+                        xaxis: {
+                            axisBorder: { show: false },
+                            axisTicks: { show: false },
+                            labels: { show: false },
+                        },
+                    }}
+                />
+            )}
+        </>
+    );
 };
 
 export default Chart;
