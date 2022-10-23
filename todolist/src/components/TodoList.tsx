f933a6184b0eb13a4e23dd9ec16837985c10aed4
weeeeey <qser155@naver.com>
2022-10-23 13:37:14 +0900

    #6.6 TodoList: useForm() : register("name"),watch("name")
    register는 input 이벤트 및 키값 관리
    watch는 등록된 인풋들에 대해 변화값 추적

--- todolist/src/components/TodoList.tsx
+++ todolist/src/components/TodoList.tsx
@@ -1,25 +1,28 @@
 import { useForm } from "react-hook-form";
 
+interface IMembershipForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    password: string;
+    password1: string;
+}
+
 const TodoList = () => {
-    const { register, watch } = useForm();
+    const { register, watch } = useForm<IMembershipForm>();
     // resister() 함수를 통해 각 입력란을 등록
     // register 함수의 결과값 name,onChange,onBlur,ref를 input element에 손쉽게 넣어줌
     // register 함수의 결과값인 required, maxLength,minLength, max,min,patter 등을 react-hook-form에게 정보를 줌으로 여러 작업 수행
+    // onBlur란 특정 input을 클릭하면 focus인거고, 바깥쪽을 누르면 그 상태를 Blur라고 함
+
+    // watch는 내가 form의 입력값들의 변화를 관찰 할 수 있게 해줌
+    // console.log(watch("email")); 빈 인자를 주면 register로 등록한 input 변화값들 다 보여줌
+    console.log(watch());
     return (
         <>
-            <form
-                onSubmit={(e) => e.preventDefault()}
-                style={{ display: "flex", flexDirection: "column" }}
-            >
-                <input
-                    {...register("email", {
-                        pattern: {
-                            value: /^[a-zA-z]*$ /,
-                            message: "비밀번호는 영어만 사용 가능",
-                        },
-                    })}
-                    placeholder="Email"
-                />
+            <form style={{ display: "flex", flexDirection: "column" }}>
+                <input {...register("email")} placeholder="Email" />
                 <input {...register("firstName")} placeholder="firstName" />
                 <input {...register("lastName")} placeholder="lastName" />
                 <input {...register("userName")} placeholder="userName" />
