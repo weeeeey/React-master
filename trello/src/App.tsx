09368dacfa7993a68c63e4b53a7d8a1f0aabb576
weeeeey <qser155@naver.com>
2022-10-25 22:18:44 +0900

    # 7.9 Same Board Drop Control

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -7,14 +7,16 @@ import Board from "./components/Board";
 const Wrapper = styled.div`
     display: flex;
     max-width: 680px;
-    width: 100%;
+    width: 100vw;
     margin: 0 auto;
     justify-content: center;
     align-items: center;
     height: 100vh;
 `;
-const Boards = styled.div`
+const DropBoards = styled.div`
     display: grid;
+    /* justify-content: center;
+    align-items: flex-start; */
     width: 100%;
     gap: 10px;
     grid-template-columns: repeat(3, 1fr);
@@ -22,8 +24,25 @@ const Boards = styled.div`
 
 const App = () => {
     const [todos, setTodo] = useRecoilState(todoState);
-    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-        if (!destination) return;
+    const onDragEnd = (info: DropResult) => {
+        // 옮기는 아이템, 도착하는 장소, 출발 장소
+        const { draggableId, destination, source } = info;
+        // destination & source 에는 index와 droppableId 정보가 담겨있음
+        // 각각의 droppableId 는 떨궈진 보드에 정보이므로
+
+        // droppableId 가 같다는 것은 출발,도착 보드가 같다는 것
+        if (destination?.droppableId === source.droppableId) {
+            setTodo((allTodos) => {
+                // 모든 보드를 다 긁어와서 해당되는 보드 전체 정보를 복사해서 조작
+                const tempTodo = [...allTodos[source.droppableId]];
+                tempTodo.splice(source.index, 1);
+                tempTodo.splice(destination.index, 0, draggableId);
+                return {
+                    ...allTodos,
+                    [source.droppableId]: tempTodo,
+                };
+            });
+        }
         // setTodo((oldTodo) => {
         //     const tempTodo = [...oldTodo];
         //     tempTodo.splice(source.index, 1);
@@ -34,7 +53,7 @@ const App = () => {
     return (
         <Wrapper>
             <DragDropContext onDragEnd={onDragEnd}>
-                <Boards>
+                <DropBoards>
                     {Object.keys(todos).map((boardId) => (
                         <Board
                             boardId={boardId}
@@ -42,7 +61,7 @@ const App = () => {
                             todos={todos[boardId]}
                         />
                     ))}
-                </Boards>
+                </DropBoards>
             </DragDropContext>
         </Wrapper>
     );
