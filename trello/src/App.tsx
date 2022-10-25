b18b693aca8761a76c6bcfacbd252c8e215d078b
weeeeey <qser155@naver.com>
2022-10-25 15:53:31 +0900

    #7.1 Atom: Set of Selector

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,12 +1,17 @@
-import { minutes, hourSelector } from "./components/atoms";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState, hourSelector } from "./components/atoms";
+import { useRecoilState } from "recoil";
 
 const App = () => {
-    const [minute, setMinutes] = useRecoilState(minutes);
-    const hour = useRecoilValue(hourSelector);
+    const [minute, setMinutes] = useRecoilState(minuteState);
+    const [hour, setHour] = useRecoilState(hourSelector);
+    // Selector 요소를 useRecoilState로 불러올 경우
+    // data 값은 get 옵션에 할당된거, setting 함수는 set 옵션에 할당된거 가져옴
 
     const onChangeMinute = (e: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+e.currentTarget.value);
+    };
+    const onChangeHour = (e: React.FormEvent<HTMLInputElement>) => {
+        setHour(+e.currentTarget.value);
     };
     return (
         <>
@@ -16,7 +21,12 @@ const App = () => {
                 type="number"
                 placeholder="Minutes"
             />
-            <input value={hour} type="number" placeholder="Hours" />
+            <input
+                value={hour}
+                onChange={onChangeHour}
+                type="number"
+                placeholder="Hours"
+            />
         </>
     );
 };
--- trello/src/components/atoms.tsx
+++ trello/src/components/atoms.tsx
@@ -1,15 +1,21 @@
 import { atom, selector } from "recoil";
 
-export const minutes = atom({
+export const minuteState = atom({
     key: "minute",
     default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
     key: "hours",
     get: ({ get }) => {
-        const minute = get(minutes);
+        const minute = get(minuteState);
 
         return minute / 60;
+    },
+    set: ({ set }, newValue) => {
+        // newValue는 string 타입으로 가져와서 number로 형 변환
+        const minutes = Number(newValue) * 60;
+        set(minuteState, minutes);
+        // 첫번쨰 인자는 atom , 두번째 인자는 수정할 값
     },
 });
