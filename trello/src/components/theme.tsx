eb566b268c301fbcd895b1efd6e2a50b60c6fa10
weeeeey <96678855+weeeeey@users.noreply.github.com>
2022-10-25 15:07:03 +0900

    Delete trello directory

--- trello/src/components/theme.tsx
+++
@@ -1,6 +0,0 @@
-import { DefaultTheme } from 'styled-components';
-
-export const darkTheme: DefaultTheme = {
-    bgColor: '#121212',
-    textColor: 'white',
-};
--- trello/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-
-// and extend them!
-declare module 'styled-components' {
-    export interface DefaultTheme {
-        bgColor: string;
-        textColor: string;
-    }
-}
