0725add3293adbeedcd1983fb1b994a9b61e1cf9
weeeeey <qser155@naver.com>
2022-10-26 13:41:07 +0900

    # 7.13 useRef 주석 추가

--- trello/src/components/Board.tsx
+++ trello/src/components/Board.tsx
@@ -40,6 +40,21 @@ const Area = styled.div<IAreaProps>`
     padding: 20px;
 `;
 
+//     useRef()
+// useRef는 .current 프로퍼티로 전달된 인자(initialValue)로 초기화된 변경 가능한 ref 객체를 반환합니다. 반환된 객체는 컴포넌트의 전 life cycle을 통해 유지될 것입니다.
+// 일반적인 사용 사례는 자식에게 접근하는 경우입니다.
+// 본질적으로 useRef는 .current 프로퍼티에 변경 가능한 값을 담고 있는 “상자”와 같습니다.
+
+// ref 속성보다 useRef()가 더 유용합니다. 이 기능은 클래스에서 인스턴스 필드를 사용하는 방법과 유사한 어떤 가변값을 유지하는 데에 편리합니다.
+// ```
+// const inputEl = useRef(null);
+
+// const onButtonClick = () => {
+// // `current` points to the mounted text input element
+// inputEl.current.focus();
+// };
+
+// < input ref={inputEl} type="text" / >
 const Board = ({ todos, boardId }: IBoardProps) => {
     const inputRef = useRef<HTMLInputElement>(null);
     // ref를 통해 HTML 요소에 접근 가능해짐
