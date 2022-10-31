825529b858ed658be8e4bbcf38f10ac6f78e1747
weeeeey <qser155@naver.com>
2022-10-31 17:52:01 +0900

    #8.7 useMotionValue 를 통해 좌표값을 추적

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { useRef } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -41,54 +41,22 @@ const boxVars = {
 };
 
 const App = () => {
-    const biggerBoxRef = useRef<HTMLDivElement>(null);
+    const x = useMotionValue(0);
+    // console.log(x) 이것을 선언하더라도 Motion은 재랜더링을 하지 않아서
+    // 콘솔창에 x 값을 보여주지 않음
+
+    useEffect(() => {
+        x.onChange(() => console.log(x.get()));
+    }, [x]);
+
     return (
         <Wrapper>
-            dragConstraints
-            {/* 허용된 드래그 가능 영역에 제약 조건을 적용합니다.
-dragConstraints 에는 드래그 가능한 컴포넌트의 가장자리 거리를 정의합니다. (드래그 가능한 영역에 가장자리에서 얼마만큼까지 허용할 것인지 지정)
-```
-// 픽셀 이용
-< motion.div drag="x" dragConstraints={{ left: 0, right: 300 }}/ >
+            {/* <SmallBox style={{x:x}} drag='x' dragSnapToOrigin /> */}
+            {/* 해당되는 컴포넌트의 x값 좌표를 계속 추적할 수 있음 */}
 
-// ref이용
-const MyComponent = () => {
-const constraintsRef = useRef(null)
-
-return (
-< motion.div ref={constraintsRef}>
-< motion.div drag dragConstraints={constraintsRef} />
-< /motion.div>
-)
-}
-```
-
-dragSnapToOrigin: boolean
-true인 경우 드래그 가능한 요소는 드래그를 놓을 때, 원점으로 다시 애니메이션됩니다.
-dragSnapToOrigin={true}
-
-dragElastic: DragElastic
-외부 제약 조건에서 허용되는 이동 정도. 0 = 움직임 없음, 1 = 전체 움직임. 기본적으로 0.5로 설정됩니다. 움직임을 비활성화하기 위해 false로 설정할 수도 있습니다.
-dragElastic={0.2}
- */}
-            {/* https://www.framer.com/docs/gestures/#drag */}
-            <BiggerBox ref={biggerBoxRef}>
-                <SmallBox
-                    // drag="x" 이렇게 지정하면 x축만 이동 가능
-                    drag
-                    // 드래그 끝나면 제자리로 돌아옴
-                    dragSnapToOrigin
-                    // 마우스 포인트를 그대로 따라오냐 마냐 조절 (0~1 사이값)
-                    // 기본값은 0
-                    dragElastic={0}
-                    // 큰박스 요소안으로 갇혀두기 위한 용도
-                    dragConstraints={biggerBoxRef}
-                    variants={boxVars}
-                    whileDrag="dragColor"
-                    whileHover="hover"
-                    whileTap="click"
-                />
-            </BiggerBox>
+            <SmallBox style={{ x }} drag="x" dragSnapToOrigin />
+            {/* x 값을 추적할 수 있으므로 세팅도 가능해짐 */}
+            <button onClick={() => x.set(-200)}>click me!</button>
         </Wrapper>
     );
 };
