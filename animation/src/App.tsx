bbec4e2c8317649847ba8fadbab30af900d1d1e8
weeeeey <qser155@naver.com>
2022-10-31 16:43:46 +0900

    dragConstraint 주석 추가

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -44,6 +44,34 @@ const App = () => {
     const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
         <Wrapper>
+            dragConstraints
+            {/* 허용된 드래그 가능 영역에 제약 조건을 적용합니다.
+dragConstraints 에는 드래그 가능한 컴포넌트의 가장자리 거리를 정의합니다. (드래그 가능한 영역에 가장자리에서 얼마만큼까지 허용할 것인지 지정)
+```
+// 픽셀 이용
+< motion.div drag="x" dragConstraints={{ left: 0, right: 300 }}/ >
+
+// ref이용
+const MyComponent = () => {
+const constraintsRef = useRef(null)
+
+return (
+< motion.div ref={constraintsRef}>
+< motion.div drag dragConstraints={constraintsRef} />
+< /motion.div>
+)
+}
+```
+
+dragSnapToOrigin: boolean
+true인 경우 드래그 가능한 요소는 드래그를 놓을 때, 원점으로 다시 애니메이션됩니다.
+dragSnapToOrigin={true}
+
+dragElastic: DragElastic
+외부 제약 조건에서 허용되는 이동 정도. 0 = 움직임 없음, 1 = 전체 움직임. 기본적으로 0.5로 설정됩니다. 움직임을 비활성화하기 위해 false로 설정할 수도 있습니다.
+dragElastic={0.2}
+ */}
+            {/* https://www.framer.com/docs/gestures/#drag */}
             <BiggerBox ref={biggerBoxRef}>
                 <SmallBox
                     // drag="x" 이렇게 지정하면 x축만 이동 가능
