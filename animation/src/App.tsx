5f32a50a67c0574485bd4c41c89b0bff2184424f
weeeeey <qser155@naver.com>
2022-10-31 16:42:15 +0900

    # 8.6 드래그 제한요소들 추가 (원상태 복귀,  포인터 움직임, 공간 제한사항)

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
@@ -10,7 +11,16 @@ const Wrapper = styled(motion.div)`
     align-items: center;
 `;
 
-const Box = styled(motion.div)`
+const BiggerBox = styled(motion.div)`
+    width: 600px;
+    height: 600px;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const SmallBox = styled(motion.div)`
     width: 200px;
     height: 200px;
     display: grid;
@@ -21,8 +31,8 @@ const Box = styled(motion.div)`
 `;
 
 const boxVars = {
-    hover: { scale: 1.5, rotateZ: 90 },
-    click: { scale: 1, borderRadius: "100px" },
+    hover: { rotateZ: 90 },
+    click: { borderRadius: "100px" },
     dragColor: {
         backgroundColor: "rgba(46,204,113)",
         // rgba값이 아닌 blue, yellow 같이 스트링 값 넣으면 애니메이션 먹힐수있음
@@ -31,18 +41,27 @@ const boxVars = {
 };
 
 const App = () => {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
-        <>
-            <Wrapper>
-                <Box
+        <Wrapper>
+            <BiggerBox ref={biggerBoxRef}>
+                <SmallBox
+                    // drag="x" 이렇게 지정하면 x축만 이동 가능
                     drag
+                    // 드래그 끝나면 제자리로 돌아옴
+                    dragSnapToOrigin
+                    // 마우스 포인트를 그대로 따라오냐 마냐 조절 (0~1 사이값)
+                    // 기본값은 0
+                    dragElastic={0}
+                    // 큰박스 요소안으로 갇혀두기 위한 용도
+                    dragConstraints={biggerBoxRef}
                     variants={boxVars}
                     whileDrag="dragColor"
                     whileHover="hover"
                     whileTap="click"
                 />
-            </Wrapper>
-        </>
+            </BiggerBox>
+        </Wrapper>
     );
 };
 
