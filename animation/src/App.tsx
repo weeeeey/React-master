8a101711f4dacf4b419f74e3d9b9d27860570744
weeeeey <qser155@naver.com>
2022-11-01 14:17:28 +0900

    # 8.10 svg 태그 props인 pathLength, fill 통해 라인 그리기

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,62 +1,77 @@
 import React from "react";
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-    height: 200vh;
+    height: 100vh;
     width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
 `;
 
-const SmallBox = styled(motion.div)`
-    width: 200px;
-    height: 200px;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 40px;
-    box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// Fontawesome Airbnb Logo
+// < />모양 클릭해서 svg복사 후 사용하시면 됩니다.
+// https://fontawesome.com/v5.15/icons/airbnb?style=brands
+
+// Line drawing
+// svg 엘리먼트에 'pathLength', 'pathSpacing', 'pathOffset' 속성을 사용하여 Line drawing 애니메이션을 만들 수 있습니다.
+// https://www.framer.com/docs/examples/#line-drawing
+
+// path (SVG)
+// path SVG 엘리먼트는 모양을 정의하는 일반 엘리먼트입니다.모든 기본 모양은 path 엘리먼트로 만들 수 있습니다.
+// path의 속성 d는 경로의 모양을 정의합니다.
+// https://developer.mozilla.org/en-US/docs/Web/SVG/Element/path
+
+// Path
+// motion.path 컴포넌트는 세 가지 강력한 SVG path 속성인 pathLength, pathSpacing 및 pathOffset을 가지고 있습니다. 수동 경로 측정이 필요 없이 모두 0과 1 사이의 값으로 설정됩니다.
+
+// Line drawing
+// 선 그리기 애니메이션은 pathLength, pathSpacing 및 pathOffset의 세 가지 특수 속성을 사용하여 많은 SVG 요소로 만들 수 있습니다.
+// ex) motion.circle initial={{ pathLength: 0 }} animate={{ pathLength: 1 }}
+// https://www.framer.com/docs/examples/#line-drawing
+
+const Svg = styled.svg`
+    width: 300px;
+    height: 300px;
+    path {
+        stroke: white;
+        stroke-width: 2;
+    }
 `;
 
-// useViewportScroll(): ScrollMotionValues
-// 뷰포트가 스크롤될 때 업데이트되는 MotionValues를 리턴합니다.
-// 아래 값들은 모두 MotionValue< number >를 넘겨줍니다.
-// scrollX: 실제 수평 스크롤 픽셀 ex) 500px
-// scrollY: 실제 수직 스크롤 픽셀 ex) 500px
-// scrollXProgress : 0 ~ 1 사이의 수평 스크롤
-// scrollYProgress : 0 ~ 1 사이의 수직 스크롤(가장 상단 0, 가장 하단 1)
-// ```
-// export const MyComponent = () => {
-// const { scrollYProgress } = useViewportScroll()
-// return < motion.div style={{ scaleX: scrollYProgress }} />
-// }
-// ```
-// https://www.framer.com/docs/motionvalue/##useviewportscroll
+const varSvg = {
+    start: {
+        pathLength: 0,
+        fill: "rgba(255,255,255,0)",
+    },
+    end: {
+        fill: "rgba(255,255,255,1)",
+        pathLength: 1,
+    },
+};
 
 const App = () => {
-    const x = useMotionValue(0);
-    const potato = useTransform(x, [-800, 0, 800], [-360, 0, 360]);
-    const gradient = useTransform(
-        x,
-        [-800, 800],
-        [
-            "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-            "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-        ]
-    );
-    const { scrollYProgress } = useScroll();
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
     return (
-        <Wrapper style={{ background: gradient }}>
-            <SmallBox
-                // smallBox에 x를 선언한건 x의 위치를 변수 x에 저장하기 위함
-                // scale은 scrollYProgress에 의존하므로 스크롤에 따라 크기 달라짐
-                style={{ x, rotateZ: potato, scale }}
-                drag="x"
-                dragSnapToOrigin
-            />
+        <Wrapper>
+            <Svg
+                focusable="false"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+            >
+                <motion.path
+                    variants={varSvg}
+                    initial="start"
+                    animate="end"
+                    transition={{
+                        // default에 5초를 줘서 라인을 그리기 끝나는건 5초이지만
+                        // fill은 디폴트 duration과는 별개로 1초안으로 채워짐 3초(delay) 후에
+                        default: { duration: 5 },
+                        fill: { duration: 1, delay: 3 },
+                    }}
+                    d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+                />
+            </Svg>
         </Wrapper>
     );
 };
