e60871d2c2010b80b8177cc43750f15d5be5a16e
weeeeey <qser155@naver.com>
2022-11-01 14:37:43 +0900

    # 8.11 컴포넌트가 사라질때 animation 특징 넣어주는 법

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
@@ -9,69 +10,74 @@ const Wrapper = styled(motion.div)`
     justify-content: center;
     align-items: center;
 `;
-
-// Fontawesome Airbnb Logo
-// < />모양 클릭해서 svg복사 후 사용하시면 됩니다.
-// https://fontawesome.com/v5.15/icons/airbnb?style=brands
-
-// Line drawing
-// svg 엘리먼트에 'pathLength', 'pathSpacing', 'pathOffset' 속성을 사용하여 Line drawing 애니메이션을 만들 수 있습니다.
-// https://www.framer.com/docs/examples/#line-drawing
-
-// path (SVG)
-// path SVG 엘리먼트는 모양을 정의하는 일반 엘리먼트입니다.모든 기본 모양은 path 엘리먼트로 만들 수 있습니다.
-// path의 속성 d는 경로의 모양을 정의합니다.
-// https://developer.mozilla.org/en-US/docs/Web/SVG/Element/path
-
-// Path
-// motion.path 컴포넌트는 세 가지 강력한 SVG path 속성인 pathLength, pathSpacing 및 pathOffset을 가지고 있습니다. 수동 경로 측정이 필요 없이 모두 0과 1 사이의 값으로 설정됩니다.
-
-// Line drawing
-// 선 그리기 애니메이션은 pathLength, pathSpacing 및 pathOffset의 세 가지 특수 속성을 사용하여 많은 SVG 요소로 만들 수 있습니다.
-// ex) motion.circle initial={{ pathLength: 0 }} animate={{ pathLength: 1 }}
-// https://www.framer.com/docs/examples/#line-drawing
-
-const Svg = styled.svg`
-    width: 300px;
-    height: 300px;
-    path {
-        stroke: white;
-        stroke-width: 2;
-    }
+const Box = styled(motion.div)`
+    width: 400px;
+    height: 200px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 40px;
+    position: absolute;
+    top: 100px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const varSvg = {
-    start: {
-        pathLength: 0,
-        fill: "rgba(255,255,255,0)",
+// AnimatePresence
+
+// AnimatePresence를 사용하면 React 트리에서 컴포넌트가 제거될 때 제거되는 컴포넌트에 애니메이션 효과를 줄 수 있습니다. React에는 다음과 같은 수명 주기 메서드가 없기 때문에 종료 애니메이션을 활성화해야 합니다.
+
+// exit
+// 이 컴포넌트가 트리에서 제거될 때 애니메이션할 대상입니다.
+// ```
+// import { motion, AnimatePresence } from "framer-motion"
+
+// export const MyComponent = ({ isVisible }) => (
+// < AnimatePresence>
+// {isVisible && (
+// initial={{ opacity: 0 }}
+// animate={{ opacity: 1 }}
+// exit={{ opacity: 0 }}
+// />
+// )}
+// < /AnimatePresence>
+// )
+// ```
+// https://www.framer.com/docs/animate-presence/
+
+const boxVar = {
+    initial: {
+        opacity: 0,
+        scale: 0,
     },
-    end: {
-        fill: "rgba(255,255,255,1)",
-        pathLength: 1,
+    visible: {
+        opacity: 1,
+        scale: 1,
+        rotateZ: 360,
+    },
+    leaving: {
+        opacity: 0,
+        scale: 0,
+        y: 50,
     },
 };
 
 const App = () => {
+    const [showing, setShowing] = useState(false);
+    const toggleShowing = () => setShowing((prev) => !prev);
     return (
         <Wrapper>
-            <Svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-            >
-                <motion.path
-                    variants={varSvg}
-                    initial="start"
-                    animate="end"
-                    transition={{
-                        // default에 5초를 줘서 라인을 그리기 끝나는건 5초이지만
-                        // fill은 디폴트 duration과는 별개로 1초안으로 채워짐 3초(delay) 후에
-                        default: { duration: 5 },
-                        fill: { duration: 1, delay: 3 },
-                    }}
-                    d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-                />
-            </Svg>
+            <button onClick={toggleShowing}>Click</button>
+            {/* AnimatePresence 는 항상 visible 해야함 (그래서 밑에 조건문을 children으로 작성 ) */}
+            {/* AnimatePresence는 안쪽에 나타나거나 사라지는게 있으면 그걸 animate 해줌 */}
+            {/* 일반 react에서는 불가능한걸 AnimatePresence로 가능하게 해주는거 */}
+            <AnimatePresence>
+                {showing ? (
+                    <Box
+                        variants={boxVar}
+                        initial="initial"
+                        animate="visible"
+                        exit="leaving"
+                    />
+                ) : null}
+            </AnimatePresence>
         </Wrapper>
     );
 };
