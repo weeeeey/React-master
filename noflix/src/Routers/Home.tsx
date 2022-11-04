5d39fbebb3e07573f035a724862cbc949829b2cc
weeeeey <qser155@naver.com>
2022-11-04 17:29:55 +0900

    #9.8 슬라이더 처리 및 이미지 삽입

--- noflix/src/Routers/Home.tsx
+++ noflix/src/Routers/Home.tsx
@@ -2,12 +2,13 @@ import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { motion, AnimatePresence, animate } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Wrapper = styled.div`
     background-color: black;
+    padding-bottom: 200px;
 `;
 const Loader = styled.div`
     display: flex;
@@ -42,44 +43,51 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
     display: grid;
-    gap: 10px;
+    gap: 5px;
     grid-template-columns: repeat(6, 1fr);
     position: absolute;
     width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
     background-color: white;
     height: 200px;
     color: red;
     font-size: 66px;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center;
 `;
 
 const varRow = {
     // +10 과 -10 을 하는건 슬라이더 마지막과 처음이 다시 만났을때 간격을 주기 위한거
     // 현자 Row에서 gap이 10px이라 저렇게 준거
-    hidden: { x: window.outerWidth + 10 },
+    hidden: { x: window.outerWidth + 5 },
     visible: { x: 0 },
-    exit: { x: -window.outerWidth - 10 },
+    exit: { x: -window.outerWidth - 5 },
 };
 
-// window.outerWidth : 브라우저 전체의 너비
-// window.outerHeight : 브라우저 전체의 높이
-// window.innerWidth : 브라우저 화면의 너비
-// window.innerHeight : 브라우저 화면의 높이
-
-// outerWidth vs innerWidth 비교 이미지
-// https://www.cluemediator.com/how-to-get-the-window-size-in-javascript
+const offset = 6;
 const Home = () => {
     const { data, isLoading } = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"],
         getMovies
     );
     const [index, setIndex] = useState(0);
-    const increaseIndex = () => {
-        setIndex((prev) => prev + 1);
-        console.log(index);
+    const [leaving, setLeaving] = useState(false);
+    const toggleLeaving = () => {
+        setLeaving((prev) => !prev);
     };
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }
+    };
+    console.log(data);
     return (
         <Wrapper>
             {isLoading ? (
@@ -93,23 +101,40 @@ const Home = () => {
                         )}
                     >
                         <Title>{data?.results[0].title}</Title>
-                        <OverView>{data?.results[0].overview}</OverView>
+                        <OverView>
+                            {data?.results[0].overview.slice(0, 200)}...
+                        </OverView>
                     </Banner>
                     <Slider>
-                        <AnimatePresence>
+                        <AnimatePresence
+                            initial={false}
+                            onExitComplete={toggleLeaving}
+                        >
                             <Row
                                 variants={varRow}
                                 initial="hidden"
                                 animate="visible"
                                 exit="exit"
-                                transition={{ type: "tween", duration: 1 }}
+                                transition={{ type: "tween", duration: 0.5 }}
                                 key={index}
-                                // key값에 index를 줬으므로 클릭으로 인해 index가 증가하면
-                                // motion은 이전 것들을 exit 해서 보내버리고 새로운 애니메이션을 보여줌
                             >
-                                {[1, 2, 3, 4, 5, 6].map((i) => (
-                                    <Box key={i}>{i}</Box>
-                                ))}
+                                {/* obj.slice(num) num갯수를 뺀 객체 반환 */}
+                                {/* obj.slice(s,e) 인덱스s부터 인덱스e 이전까지 반환 */}
+                                {data?.results
+                                    .slice(1)
+                                    .slice(
+                                        offset * index,
+                                        offset * index + offset
+                                    )
+                                    .map((movie) => (
+                                        <Box
+                                            key={movie.id}
+                                            bgPhoto={makeImagePath(
+                                                movie.poster_path,
+                                                "w500"
+                                            )}
+                                        />
+                                    ))}
                             </Row>
                         </AnimatePresence>
                     </Slider>
