dd63578d61567848cf554bac98a95f4d4f20f3d0
weeeeey <qser155@naver.com>
2022-11-05 16:07:04 +0900

    #9.10 슬라이더 박스 hover시 부가 정보 뜨게 하기

--- noflix/src/Routers/Home.tsx
+++ noflix/src/Routers/Home.tsx
@@ -49,16 +49,6 @@ const Row = styled(motion.div)`
     width: 100%;
 `;
 
-const Box = styled(motion.div)<{ bgPhoto: string }>`
-    background-color: white;
-    height: 200px;
-    color: red;
-    font-size: 66px;
-    background-image: url(${(props) => props.bgPhoto});
-    background-size: cover;
-    background-position: center;
-`;
-
 const varRow = {
     // +10 과 -10 을 하는건 슬라이더 마지막과 처음이 다시 만났을때 간격을 주기 위한거
     // 현자 Row에서 gap이 10px이라 저렇게 준거
@@ -67,18 +57,57 @@ const varRow = {
     exit: { x: -window.outerWidth - 5 },
 };
 
+const Box = styled(motion.div)<{ bgphoto: string }>`
+    background-color: white;
+    height: 200px;
+    color: red;
+    font-size: 66px;
+    background-image: url(${(props) => props.bgphoto});
+    background-size: cover;
+    background-position: center;
+    &:first-child {
+        transform-origin: center left;
+    }
+    &:last-child {
+        transform-origin: center right;
+    }
+`;
+
 const varBox = {
     hover: {
         scale: 1.3,
-        y: -50,
+        y: -80,
         transition: {
             delay: 0.5,
-            duration: 0.3,
+            duration: 0.5,
             type: "tween",
         },
     },
 };
 
+const Info = styled(motion.div)`
+    padding: 10px;
+    background-color: ${(props) => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    h4 {
+        text-align: center;
+        font-size: 18px;
+    }
+`;
+
+const varInfo = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.1,
+            type: "tween",
+        },
+    },
+};
 const offset = 6;
 const Home = () => {
     const { data, isLoading } = useQuery<IGetMoviesResult>(
@@ -138,7 +167,7 @@ const Home = () => {
                                     .map((movie) => (
                                         <Box
                                             key={movie.id}
-                                            bgPhoto={makeImagePath(
+                                            bgphoto={makeImagePath(
                                                 movie.poster_path,
                                                 "w500"
                                             )}
@@ -146,7 +175,13 @@ const Home = () => {
                                             variants={varBox}
                                             initial={{ scale: 1 }}
                                             whileHover="hover"
-                                        />
+                                        >
+                                            {/* 부모 컴포넌트 variants에는 hover가 있으므로 자식에게도
+                                            자동으로 whilehover="hover"가 적용됨 */}
+                                            <Info variants={varInfo}>
+                                                <h4>{movie.title}</h4>
+                                            </Info>
+                                        </Box>
                                     ))}
                             </Row>
                         </AnimatePresence>
